657dd9c482c930bcebe6ae91ade0d5e5c515d7a4
Jun <watermoonxx@gmail.com>
2023-05-25 09:40:06 +0900

    변수 이름 수정

--- src/main/java/user/UserDAO.java
+++ src/main/java/user/UserDAO.java
@@ -8,7 +8,15 @@ public class UserDAO {
 	
 	// 생성자: 객체가 만들어지자마자 데이터베이스에 접속하도록(?)  
 	public UserDAO() {
-		pool = DBConnectionMgr.getInstance(); // DBConnectionMgr 객체의 인스턴스를 가져옴 
+		try {
+			pool = DBConnectionMgr.getInstance(); // DBConnectionMgr 객체의 인스턴스를 가져옴 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		
 	}
 	
 	// login 메소드
@@ -45,15 +53,18 @@ public class UserDAO {
 		
 		try {
 			con = pool.getConnection(); // 커넥션 풀 접근 
-			String sql = "SELECT * FROM USER1 WHERE USERID = ?";
+			String sql = "SELECT * FROM USER1 WHERE userID = ?";
 			pstmt = con.prepareStatement(sql);
 			pstmt.setString(1, userID);
 			rs = pstmt.executeQuery();
 			
 			if (rs.next()) {
-				if (rs.getString("userPassword1").equals(userPwd)) { // 사용자가 입력한 userPwd와 데이터베이스의 userPwd가 동일하다면
+				if (rs.getString("userPwd").equals(userPwd)) { 
+					// 사용자가 입력한 userPwd와 데이터베이스의 userPwd가 동일하다면
+					// getString("userPwd") -> 데이터베이스의 컬럼 접근 
+					
 					return 1; // 로그인 성공 
-				} 
+				}
 				return 2; // 비밀번호 일치하지 않음 
 			} else { // 사용자 존재하지 않는다면 
 				return 0; 
@@ -75,7 +86,7 @@ public class UserDAO {
 		
 		try {
 			con = pool.getConnection(); // 커넥션 풀 접근 
-			String sql = "SELECT * FROM USER1 WHERE USERID = ?";
+			String sql = "SELECT * FROM USER1 WHERE userID = ?";
 			pstmt = con.prepareStatement(sql);
 			pstmt.setString(1, userID);
 			rs = pstmt.executeQuery();
@@ -83,7 +94,7 @@ public class UserDAO {
 			if (rs.next() || userID.equals("")) { // 아이디가 이미 존재하거나, 공백일경우 
 				return 0; // 이미 존재하는 회원 
 			} else { // 그렇지 않은 경우 
-				return 1; // 가입 가능한 회원 아이디 
+				return 1; // 가입 가능한 아이디 
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -94,31 +105,93 @@ public class UserDAO {
 	} // end of registerCheck() 
 	
 	// 회원가입시도 메소드 
-		public int register(String userID, String userPwd, String userName, String phone, String zipcode, String address, String detailAddress, String userProfile) {
-			Connection con = null;
-			PreparedStatement pstmt = null;
-			ResultSet rs = null;
+	public int register(String userID, String userPwd, String userName, String phone, String zipcode, String address, String detailAddress, String userProfile) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			con = pool.getConnection(); // 커넥션 풀 접근 
+			String sql = "INSERT INTO USER1 VALUES(?,?,?,?,?,?,?,?)";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, userID);
+			pstmt.setString(2, userPwd);
+			pstmt.setString(3, userName);
+			pstmt.setString(4, phone);
+			pstmt.setString(5, zipcode);
+			pstmt.setString(6, address);
+			pstmt.setString(7, detailAddress);
+			pstmt.setString(8, userProfile);
+			return pstmt.executeUpdate(); // 조회가 아닌 삽입이므로(SELECT문이 아니라 INSERT문) executeQuery()가 아니라 executeUpdate() [5강]   
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt);
+		}
+		return -1; // 데이터베이스 오류 
+	} // end of register() 
+		
+		
+	// userID에 해당하는 사용자 정보를 가져오는 함수 
+	// user를 반환하므로 반환 타입은 UserDTO 
+	public UserDTO getUser(String userID) {
+		UserDTO user = new UserDTO();
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			con = pool.getConnection(); // 커넥션 풀 접근 
+			String sql = "SELECT * FROM USER1 WHERE userID = ?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, userID);
+			rs = pstmt.executeQuery();
 			
-			try {
-				con = pool.getConnection(); // 커넥션 풀 접근 
-				String sql = "INSERT INTO USER1 VALUES(?,?,?,?,?,?,?,?)";
-				pstmt = con.prepareStatement(sql);
-				pstmt.setString(1, userID);
-				pstmt.setString(2, userPwd);
-				pstmt.setString(3, userName);
-				pstmt.setString(4, phone);
-				pstmt.setString(5, zipcode);
-				pstmt.setString(6, address);
-				pstmt.setString(7, detailAddress);
-				pstmt.setString(8, userProfile);
-				return pstmt.executeUpdate(); // 조회가 아닌 삽입이므로(SELECT문이 아니라 INSERT문) executeQuery()가 아니라 executeUpdate() [5강]   
-			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
-				pool.freeConnection(con, pstmt);
+			if (rs.next()) { 
+				user.setUserID(userID);
+				user.setUserPwd(rs.getString("userPwd"));
+				user.setUserName(rs.getString("userName"));
+				user.setPhone(rs.getString("phone"));
+				user.setZipcode(rs.getString("zipcode"));
+				user.setAddress(rs.getString("address"));
+				user.setDetailAddress(rs.getString("detailAddress"));
+				user.setUserProfile(rs.getString("userProfile"));
+				// 데이터를 참조 변수 user에 저장 
 			}
-			return -1; // 데이터베이스 오류 
-		} // end of register() 
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt, rs);
+		}
+		return user;
+	} // end of getUser() 
+		
+	// 회원정보수정 함수 -> 비밀번호, 이름, 연락처, 주소만 변경 가능하게 처리 
+	public int update(String userID, String userPwd, String userName, String phone, String zipcode, String address, String detailAddress) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			con = pool.getConnection(); // 커넥션 풀 접근 
+			String sql = "UPDATE USER1 SET userPwd = ?, userName = ?, phone = ?, zipcode = ?, address = ?, detailAddress = ? WHERE userID = ?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, userPwd);
+			pstmt.setString(2, userName);
+			pstmt.setString(3, phone);
+			pstmt.setString(4, zipcode);
+			pstmt.setString(5, address);
+			pstmt.setString(6, detailAddress);
+			pstmt.setString(7, userID); 
+			
+			return pstmt.executeUpdate(); 
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt);
+		}
+		return -1; // 데이터베이스 오류 
+	} // end of update() 
 	
 	
 
